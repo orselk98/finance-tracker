52d553a4455706e6cf888701199eb000145c5185
Orsel Kapllani <orsel.a.kapllani@gmail.com>
2026-01-12 21:30:08 +0100

    Added CreditCard URLs and completed API

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import all_transactions, transaction_detail
+from .views import all_transactions, transaction_detail, creditcard_detail, all_creditcards
 
 urlpatterns = [
     path('transactions/', all_transactions, name='transactions_list'),
     path('transactions/<int:id>/', transaction_detail, name='transaction-detail'),
+    path('creditcards/', all_creditcards,name='creditcards_list'),
+    path('creditcards/<int:number>/', creditcard_detail, name='creditcard-detail')
+
 ]
